1edc078eec4d3c64f3290400b25268b54130023f
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-29 16:10:20 +0600

    Component Tree

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+
+import Header from "./header/Header"
+import Body from "./body/Body"
+import Footer from "./footer/Footer"
+
+const MainComponent = () => {
+    return (
+        <div>
+            <h1>Main Component</h1>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
+
+export default MainComponent;
